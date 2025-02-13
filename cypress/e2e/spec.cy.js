acdbc2ad8be1ac0c59ecea7a755b573e8c59d8c6
Anderson Soares <anderson.soares@tistech.co.ao>
2025-02-13 17:38:39 +0100

    terceiro commit

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,21 +3,22 @@ describe("template spec", () => {
     cy.log("Executando antes de todos os testes");
     cy.visit("https://bugbank.netlify.app/");
   });
-  it("Cadastrar usuario", () => {
-    cy.contains("Registrar").click();
-    cy.get('input[name="email"]').eq(1).type("teste@teste.com", {force: true});
-    cy.get('input[name="name"]').type("Teste", {force: true});
-    cy.get('input[name="password"]').eq(1).type("test321", {force: true});
-    cy.get('input[name="passwordConfirmation"]').type("test321", {force: true});
-    cy.contains('button', 'Cadastrar').click({force: true});
-    cy.get('a#btnCloseModal').click();
+  it("Cadastrar usuario e Logar no sistema", () => {
+    cy.get(".ihdmxA").click();
+    cy.get('input[name="email"]')
+      .eq(1)
+      .type("teste@teste.com", { force: true });
+    cy.get('input[name="name"]').type("Teste", { force: true });
+    cy.get('input[name="password"]').eq(1).type("test321", { force: true });
+    cy.get('input[name="passwordConfirmation"]').type("test321", {
+      force: true,
+    });
+    cy.contains("button", "Cadastrar").click({ force: true });
+    cy.get("a#btnCloseModal").click();
     cy.get('input[name="email"]').eq(0).type("teste@teste.com");
-    cy.get('input[name="password"]').eq(0).type("Teste");
-    cy.contains('button', 'Acessar').click();
-    
-  });
-  it('Logar', () => {
-    cy.visit('https://bugbank.netlify.app/home')
-    cy.get('img[data-nimg="intrinsic"]').eq(0).should('be.visible');
+    cy.get('input[name="password"]').eq(0).type("test321");
+    cy.contains("button", "Acessar").click();
+    cy.url().should('include', '/home');
+    cy.get('img[data-nimg="intrinsic"]').eq(0).should("be.visible");
   });
 });
